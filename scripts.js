e24f0f24a2bbc1521c7ab7a3cefd0536aafc57f5
John Park <jwpark417@gmail.com>
2016-06-23 17:29:35 -0400

    Add create/post functionality

--- scripts.js
+++ scripts.js
@@ -1,3 +1,47 @@
 $(function() {
-  
+  //GET/READ
+  $('#get-button').on('click', function() {
+    $.ajax({
+      url: '/products',
+      contentType: 'application/json',
+      success: function(response) {
+        var tbodyEl = $('tbody');
+
+        tbodyEl.html('');
+
+        response.products.forEach(function(product) {
+          tbodyEl.append('\
+            <tr>\
+              <td class="id">' + product.id +'</td>\
+              <td><input type="text" class="name" value="' + product.name + '"></td>\
+              <td>\
+                <button class="update-button">UPDATE/PUT</button>\
+                <button class="delete-button">DELETE</button>\
+              </td>\
+            </tr>\
+          ');
+        })
+      }
+    })
+  });
+
+  //CREATE/POST
+  $('#create-form').on('submit', function(event) {
+    event.preventDefault();
+
+    var createInput = $('#create-input');
+
+    $.ajax({
+      url: '/products',
+      method: 'POST',
+      contentType: 'application/json',
+      data: JSON.stringify({ name: createInput.val() }),
+      success: function(response) {
+        console.log(response);
+        createInput.val('');
+        $('#get-button').click();
+      }
+    });
+  });
+
 });
